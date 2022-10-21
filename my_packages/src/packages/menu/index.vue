cb763ec400cccdee73c3a30ae56ec83081bfe333
danhauxiansheng <646042371@qq.com>
2022-10-21 17:07:27 +0800

    【*】提交基础示例

--- my_packages/src/packages/menu/index.vue
+++ my_packages/src/packages/menu/index.vue
@@ -1,13 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="z-menu flex">
+    <!-- <router-link class="z-menu-item" to="/">
+      <span class="titleSpan">首页</span>
+    </router-link>
+    <router-link class="z-menu-item" to="/project-detail-all">
+      <span class="titleSpan">项目</span>
+    </router-link> -->
+    <ul class="z-menu flex">
+      <li
+        v-for="item in dataList"
+        :key="item.title"
+        :class="{ active: item.active }"
+        @mouseover="item.active = true"
+        @mouseout="item.active = false"
+        class="z-menu-item"
+      >
+        <a class="router-name">{{ item.title }}</a>
+        <template v-if="item.children && item.children.length">
+          <dl
+            v-show="item.active"
+            v-for="child in item.children"
+            :key="child.title"
+          >
+            <dt>
+              <span class="router-name">{{ child.title }}</span>
+            </dt>
+            <template v-if="child.children && child.children.length">
+              <dd v-for="lastChild in child.children" :key="lastChild.title">
+                <span class="router-name">{{ lastChild.title }}</span>
+              </dd>
+            </template>
+          </dl>
+        </template>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+// 通过ref设置数据为响应式
+import { ref } from "vue";
+
+const dataList = ref([
+  {
+    title: "title1",
+    router: "/",
+    icon: null,
+    class: null,
+    children: [
+      {
+        title: "child1-1",
+        router: "/",
+        children: [
+          { title: "child1-1-1", router: "/", icon: null, class: null },
+          { title: "child1-1-2", router: "/", icon: null, class: null },
+          { title: "child1-1-3", router: "/", icon: null, class: null },
+          { title: "child1-1-4", router: "/", icon: null, class: null },
+        ],
+      },
+      { title: "child1-2", router: "/", icon: null, class: null },
+      { title: "child1-3", router: "/", icon: null, class: null },
+      { title: "child1-4", router: "/", icon: null, class: null },
+    ],
+  },
+  { title: "title2", router: "/", icon: null, class: null },
+  { title: "title3", router: "/", icon: null, class: null },
+  { title: "title4", router: "/", icon: null, class: null },
+]);
+
+// function setActiveStatus(data, status) {
+//   data.active = status;
+// }
+</script>
 
 <script>
 export default {
@@ -19,4 +83,31 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style lang="less" scoped>
+.flex {
+  display: flex;
+  align-items: center;
+}
+
+.z-menu {
+  height: 64px;
+  margin: 0px;
+  .router-name {
+    line-height: 64px;
+    font-size: 16px;
+    margin: 0 30px;
+    height: 100%;
+    display: block;
+    cursor: pointer;
+  }
+  .active {
+    color: #00d2ff;
+    border-bottom: 3px solid #00d2ff;
+  }
+
+  .z-menu-item {
+    line-height: 64px;
+    list-style: none;
+  }
+}
+</style>
